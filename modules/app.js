3ade63e1743b56942f17be546bb7f666a4d5ead3
Chi Kei Chan <chikeichan@gmail.com>
2014-11-06 21:14:32 -0800

    Experiment with AngularJS

---
+++ modules/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/accounting/journal', {
+			controller: 'controller',
+			templateUrl: '../views/journal.html'
+		})
+		.when('/accounting/', {
+			templateUrl: '../views/accountingView.html'
+		})
+		.when('/reports', {
+			templateUrl: '../views/reportView.html'
+		})
+		.otherwise({redirectTo:'/'});
+});
+
+var transactions=[];
+
+app.controller('controller', function($scope){
+	$scope.transactions = transactions;
+	$scope.addTransaction = function(){
+		var transaction = {
+				gl: $scope.gl,
+				debit: $scope.debit,
+				credit: $scope.credit,
+				desc: $scope.desc,
+
+				gl2: $scope.gl2,
+				debit2: $scope.debit2,
+				credit2: $scope.credit2,
+				desc2: $scope.desc2
+			}
+			transactions.push(transaction);
+		}
+
+});
---
+++ modules/header.js
@@ -0,0 +1,16 @@
+/*
+$(document).ready(function(){
+	$('nav.navbar>div#tab').mouseenter(function(){
+		$(this).animate({
+			top: '0px',
+			left: '5px'
+		},200);
+	});
+	$('nav.navbar>div#tab').mouseleave(function(){
+		$(this).animate({
+			top: '-26px',
+			left: '0px'
+		},400);
+	})
+})
+*/
