11b24d1cbda5c01f21caf407a02a1c9d9470269e
Chi Kei Chan <chikeichan@gmail.com>
2014-11-06 16:01:59 -0800

    Basic layout and some experimenting codes for AngularJS

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//app.js
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			controller: 'controller',
+			templateUrl: './accountingView.html'
+		})
+		.otherwise({redirectTo:'/'});
+});
+
+app.controller('controller', function($scope){
+	$scope.testing = [1,2,3,4,5];
+});
